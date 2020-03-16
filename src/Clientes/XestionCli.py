f32be43f3e508802718cbdeb54536558b8fc2cda
ecollazodominguez <ecollazodominguez@danielcastelao.org>
2020-03-17 00:52:42 +0100

    Interfaz Xestion Clientes

--- src/Clientes/XestionCli.py
+++ src/Clientes/XestionCli.py
@@ -8,14 +8,21 @@ class Fiestra(Gtk.Window):
 
     def __init__(self):  # constructor
         Gtk.Window.__init__(self, title="Xestión de Clientes")
-        self.set_default_size(400, 300)
+        self.set_default_size(600, 400)
 
         boxV = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
         self.modelo = Gtk.ListStore(str, str, str, str, str, str)
-        self.modelo.append(["532423F", "Alfredo", "Dominguez", "M", "986172748", "Garcia Barbon"])
+        self.modelo.append(["53242337F", "Alfredo", "Dominguez", "M", "986172748", "Garcia Barbon"])
+        self.modelo.append(["93758295N", "Maria", "Garzon", "F", "986352378", "Zaragoza"])
+        self.modelo.append(["58394052G", "Eugenia", "Val", "F", "986642347", "Valencia"])
+        self.modelo.append(["28503758L", "Eduardo", "Collazo", "M", "986152764", "Pintor Colmeiro"])
+
+
 
         vista = Gtk.TreeView(model=self.modelo)
+        vista.set_hexpand(True)
+        vista.set_vexpand(True)
         seleccion = vista.get_selection()
        # seleccion.connect("changed", self.on_vista_changed)
         boxV.pack_start(vista, True, True, 0)
@@ -24,7 +31,6 @@ class Fiestra(Gtk.Window):
         celdaDNI.set_property("editable", False)  # con false no se pueden editar las celdas
 
         columnaDNI = Gtk.TreeViewColumn('DNI', celdaDNI, text=0)
-
         celdaNome = Gtk.CellRendererText()
         celdaNome.set_property("editable", False)
        # celdaNome.connect("edited", self.on_celdaDireccion_edited, self.modelo)
@@ -54,37 +60,58 @@ class Fiestra(Gtk.Window):
         vista.append_column(columnaTelefono)
         vista.append_column(columnaDireccion)
 
-        self.add(boxV)  # al añadir el boxH esta línea sobra
+       # self.add(boxV)  # al añadir el boxH esta línea sobra
+
+        grid = Gtk.Grid(column_homogeneous=True,
+                         column_spacing=10,
+                         row_spacing=10)
+
+        boxH = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.lblDni = Gtk.Label("DNI:")
+        self.txtDni = Gtk.Entry()
+        self.lblNome = Gtk.Label("Nome:")
+        self.txtNome = Gtk.Entry()
+        self.lblApelido = Gtk.Label("Apelido:")
+        self.txtApelido = Gtk.Entry()
+        self.lblSexo = Gtk.Label("Sexo:")
+        self.txtSexo = Gtk.Entry()
+        self.lblTelefono = Gtk.Label("Telefono:")
+        self.txtTelefono = Gtk.Entry()
+        self.lblDireccion = Gtk.Label("Dirección:")
+        self.txtDireccion = Gtk.Entry()
+        self.btnAplicar = Gtk.Button(label = "Aplicar")
+        self.cmbAccion = Gtk.ComboBox()
+        acciones = Gtk.ListStore(int,str)
+        acciones.append([1,"Añadir"])
+        acciones.append([2,"Modificar"])
+        acciones.append([3,"Eliminar"])
+        self.cmbAccion.set_model(acciones)
+        celdaTexto = Gtk.CellRendererText()
+        self.cmbAccion.pack_start(celdaTexto,True)
+        self.cmbAccion.add_attribute(celdaTexto, "text", 1)
+        self.cmbAccion.set_active(0)
+
+        grid.add(self.lblDni)
+        grid.attach(self.txtDni,0,1,1,1)
+        grid.attach_next_to(self.lblNome,self.lblDni,Gtk.PositionType.RIGHT,1,1)
+        grid.attach(self.txtNome, 1, 1, 1, 1)
+        grid.attach_next_to(self.lblApelido,self.lblNome,Gtk.PositionType.RIGHT,1,1)
+        grid.attach(self.txtApelido, 2, 1, 1, 1)
+        grid.attach_next_to(self.lblSexo,self.txtDni,Gtk.PositionType.BOTTOM,1,1)
+        grid.attach(self.txtSexo, 0, 3, 1, 1)
+        grid.attach_next_to(self.lblTelefono,self.lblSexo,Gtk.PositionType.RIGHT,1,1)
+        grid.attach(self.txtTelefono, 1, 3, 1, 1)
+        grid.attach_next_to(self.lblDireccion,self.lblTelefono,Gtk.PositionType.RIGHT,1,1)
+        grid.attach(self.txtDireccion, 2, 3, 1, 1)
+        grid.attach(self.cmbAccion,1,4,1,1)
+        grid.attach(self.btnAplicar,2,5,1,1)
+
+
+        boxV.pack_start(grid, True, True, 10)
+
+        self.add(boxV)
         self.show_all()
         self.connect("destroy", Gtk.main_quit)
-"""
-        boxH = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.txtHotel = Gtk.Entry()
-        self.txtDireccion = Gtk.Entry()
-        self.txtOcupacion = Gtk.Entry()
-        self.chkMascota = Gtk.CheckButton()
-        self.cmbCategoria = Gtk.ComboBox()
-        btnNovo = Gtk.Button("Novo")
-        btnNovo.connect("clicked", self.on_btnNovo_clicked, self.modelo)
-        boxH.pack_start(self.txtHotel, True, False, 0)
-        boxH.pack_start(self.txtDireccion, True, False, 0)
-        boxH.pack_start(self.txtOcupacion, True, False, 0)
-        boxH.pack_start(self.chkMascota, True, False, 0)
-        boxH.pack_start(self.cmbCategoria, True, False, 0)
-        boxH.pack_start(btnNovo, True, False, 0)
-        boxV.pack_start(boxH, True, False, 0)
-        caixaFiltro = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.chkFiltro = Gtk.CheckButton(label='Filtro ocupación')
-        self.chkFiltro.connect("toggled", self.on_chkFiltro_toggled, modeloFiltrado)
-        caixaFiltro.pack_start(self.chkFiltro, True, True, 0)
-
-        boxV.pack_start(caixaFiltro, True, False, 0)
-        self.add(boxV)
-
-        self.cmbCategoria.set_model(modeloCat)
-        self.cmbCategoria.pack_start(celdaText, True)
-        self.cmbCategoria.add_attribute(celdaText, "text", 0)
-"""
 
 """
 
